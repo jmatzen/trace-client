02a2853091a3ea3b031e848b4dd19548809ab217
John Matzen <john@ayxia.com>
2016-05-19 19:06:02 -0500

    added missing 64-bit ints to argtype map

--- include/trace/trace.h
+++ include/trace/trace.h
@@ -148,9 +148,12 @@ namespace ayxia
     AYX_ARGTYPE(uint16_t, att_uint16);
     AYX_ARGTYPE(int32_t, att_int32);
     AYX_ARGTYPE(uint32_t, att_uint32);
+    AYX_ARGTYPE(int64_t, att_int64);
+    AYX_ARGTYPE(uint64_t, att_uint64);
     AYX_ARGTYPE(float, att_float32);
     AYX_ARGTYPE(double, att_float64);
     AYX_ARGTYPE(const char*, att_string);
+    AYX_ARGTYPE(char*, att_string);
     AYX_ARGTYPE(const wchar_t*, att_wstring);
     template<typename T, int N>
     struct argtype<T[N]> {
