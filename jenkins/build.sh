5e1bbbfda161f7b3fbdc7b2b27fac3a0fc197f8f
John Matzen <john@ayxia.com>
2016-08-06 15:07:05 -0500

    build issue cleaning out build

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,24 +5,26 @@ echo "updating git modules"
 git submodule init
 git submodule update
 echo "building..."
-rm -rf build
-mkdir build
-cd build
 
 build () {
-	echo building $1
-	if [ $1 == 'x86_64' ]; then
-		cmake -G "Visual Studio 14 2015 Win64" ..
-	elif [ $1 == 'x86' ]; then
-		cmake -G "Visual Studio 14 2015" ..
-	fi
-	msbuild.exe trace-client.sln /p:Configuration=Release;OutputPath=foo
-	mkdir -p ../dist/bin/$1
-	cp bin/Release/ayxiatrace.dll ../dist/bin/$1
-	cp bin/Release/sample_*.exe ../dist/bin/$1
-	mkdir -p ../dist/lib/$1
-	cp bin/Release/ayxiatrace.lib ../dist/lib/$1
-	cp bin/Release/libuv.lib ../dist/lib/$1
+	(
+		rm -rf build
+		mkdir build
+		cd build
+		echo building $1
+		if [ $1 == 'x86_64' ]; then
+			cmake -G "Visual Studio 14 2015 Win64" ..
+		elif [ $1 == 'x86' ]; then
+			cmake -G "Visual Studio 14 2015" ..
+		fi
+		msbuild.exe trace-client.sln /p:Configuration=Release;OutputPath=foo
+		mkdir -p ../dist/bin/$1
+		cp bin/Release/ayxiatrace.dll ../dist/bin/$1
+		cp bin/Release/sample_*.exe ../dist/bin/$1
+		mkdir -p ../dist/lib/$1
+		cp bin/Release/ayxiatrace.lib ../dist/lib/$1
+		cp bin/Release/libuv.lib ../dist/lib/$1
+	)
 }
 
 build x86
